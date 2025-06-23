09413ed40e16278e5924947d83ea9efbb8d32941
Tanvir Ahmed <110832885+tanvir-talha058@users.noreply.github.com>
2025-06-23 23:06:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.6)
 mp_draw = mp.solutions.drawing_utils
 
+
 # Smooth movement parameters
 smoothening = 5
 prev_x, prev_y = 0, 0
